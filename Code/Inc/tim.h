fb67f26c875ea48b36ede5b749fc9b0642a5a3ee
HWeigo <746144870@qq.com>
2019-05-30 22:28:24 +0800

    舵机PWM输出控制转移至TIM2ch3(PA2)

--- Code/Inc/tim.h
+++ Code/Inc/tim.h
@@ -62,7 +62,6 @@
 /* USER CODE END Includes */
 
 extern TIM_HandleTypeDef htim2;
-extern TIM_HandleTypeDef htim3;
 extern TIM_HandleTypeDef htim4;
 extern TIM_HandleTypeDef htim14;
 
@@ -73,7 +72,6 @@ extern TIM_HandleTypeDef htim14;
 extern void _Error_Handler(char *, int);
 
 void MX_TIM2_Init(void);
-void MX_TIM3_Init(void);
 void MX_TIM4_Init(void);
 void MX_TIM14_Init(void);
                     
--- Code/Src/stm32f4xx_it.c
+++ Code/Src/stm32f4xx_it.c
@@ -41,7 +41,6 @@
 /* USER CODE END 0 */
 
 /* External variables --------------------------------------------------------*/
-extern TIM_HandleTypeDef htim3;
 extern DMA_HandleTypeDef hdma_usart1_rx;
 
 extern TIM_HandleTypeDef htim1;
@@ -181,20 +180,6 @@ void TIM1_UP_TIM10_IRQHandler(void)
   /* USER CODE BEGIN TIM1_UP_TIM10_IRQn 1 */
 
   /* USER CODE END TIM1_UP_TIM10_IRQn 1 */
-}
-
-/**
-* @brief This function handles TIM3 global interrupt.
-*/
-void TIM3_IRQHandler(void)
-{
-  /* USER CODE BEGIN TIM3_IRQn 0 */
-
-  /* USER CODE END TIM3_IRQn 0 */
-  HAL_TIM_IRQHandler(&htim3);
-  /* USER CODE BEGIN TIM3_IRQn 1 */
-
-  /* USER CODE END TIM3_IRQn 1 */
 }
 
 /**
