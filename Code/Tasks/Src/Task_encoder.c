3b2d389ec7c8a1b00f060109e35637dc1b73de10
HWeigo <746144870@qq.com>
2019-06-05 21:08:44 +0800

    TIM3编码器调通

--- Code/Tasks/Src/Task_encoder.c
+++ Code/Tasks/Src/Task_encoder.c
@@ -10,9 +10,10 @@
 
 
 /*************编码器计数*****/
-int32_t encoder = 0;
+int32_t encoderL= 0, encoderR = 0;
 
-uint32_t encoderDebug = 0,encoderCnt = 0, tmpEncoder = 0;
+uint32_t encoderDebug = 0,encoderCnt = 0;
+uint32_t tmpEncoderL = 0;
 void Task_encoder(void const * argument)
 {
 
@@ -27,11 +28,16 @@ void Task_encoder(void const * argument)
 			encoderCnt=0;
 			++encoderDebug;
 		}
-		tmpEncoder = __HAL_TIM_GetCounter(&htim5);
-		if(__HAL_TIM_DIRECTION_STATUS(&htim5)==0)	encoder=encoder+__HAL_TIM_GetCounter(&htim5);
-		else if(__HAL_TIM_GetCounter(&htim5)!=0)	encoder=encoder-(0xFFFF-__HAL_TIM_GetCounter(&htim5)+1);
-		//printf("%d\n",i);
+		//tmpEncoderL = __HAL_TIM_GetCounter(&htim5);
+		
+		if(__HAL_TIM_DIRECTION_STATUS(&htim5)==0)	encoderL=encoderL+__HAL_TIM_GetCounter(&htim5);
+		else if(__HAL_TIM_GetCounter(&htim5)!=0)	encoderL=encoderL-(0xFFFF-__HAL_TIM_GetCounter(&htim5)+1);
+		if(__HAL_TIM_DIRECTION_STATUS(&htim3)==0)	encoderR=encoderR+__HAL_TIM_GetCounter(&htim3);
+		else if(__HAL_TIM_GetCounter(&htim3)!=0)	encoderR=encoderR-(0xFFFF-__HAL_TIM_GetCounter(&htim3)+1);		
+		
+		
 		__HAL_TIM_SetCounter(&htim5,0);
+		__HAL_TIM_SetCounter(&htim3,0);
 		
 		vTaskDelayUntil( &xLastWakeTime, ( 10 / portTICK_RATE_MS ) ); //进入阻塞态50ms
     //osDelay(1);
